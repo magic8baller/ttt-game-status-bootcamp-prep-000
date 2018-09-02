e1a6c37b5074332bceda6b1451933ff004107163
Leslie Turis <lturis@gmail.com>
2018-09-02 20:20:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,23 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], #top row
+  [3, 4, 5],  #middle row
+  [6, 7, 8], #bottom row
+  [0, 3, 6],  #left column
+  [1, 4, 7],  #middle column
+  [2, 5, 8],  #right column
+  [0, 4, 8], #top-L to bottom-R diagonal
+  [2, 4, 6],  #top-R to bottom-L diagonal
+]
+
+def won?(board)
+
+  #empty_board = board.all? { |i| i = " " }
+  WIN_COMBINATIONS.each do |win_combo|
+    win_index_1 = win_combo[0]
+    win_index_2 = win_combo[1]
+    win_index_3 = win_combo[2]
+  end
+end
