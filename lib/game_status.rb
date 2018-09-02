898c036650f24289b0a5457254089ae0980fec82
Leslie Turis <lturis@gmail.com>
2018-09-02 21:46:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,8 +18,8 @@ WIN_COMBINATIONS = [
 def won?(board)
 
   #empty_board = board.all? { |i| i = " " }
-  WIN_COMBINATIONS.each.with_index do |win_combo, i|
-    win_index = win_combo[0]
+  WIN_COMBINATIONS.each.with_index do |win_combo|
+    win_index_1 = win_combo[0]
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[2]
 
