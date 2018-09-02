65aa2d7da236b4d3a634c3882148fe8c0a713eb8
Leslie Turis <lturis@gmail.com>
2018-09-02 21:45:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,10 @@ def won?(board)
   #empty_board = board.all? { |i| i = " " }
   WIN_COMBINATIONS.each.with_index do |win_combo, i|
     win_index = win_combo[i]
-    position = board[win_index]
+  
+    position = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
     if position_taken?(board, index)
       return win_combo
