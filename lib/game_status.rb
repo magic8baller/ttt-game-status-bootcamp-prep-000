6eb0b715fb24f04a360ce3f701c2287d88223706
Leslie Turis <lturis@gmail.com>
2018-09-02 20:24:22 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,5 +22,14 @@ def won?(board)
     win_index_1 = win_combo[0]
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == 'X' && position_2 == 'X' && position_3 == 'X'
+      return win_combo
+    else
+      false
   end
 end
